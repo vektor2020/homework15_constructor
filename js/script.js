b755838bd7ae043ba4f0f1e2bd46b017029cddc9
vektor2020 <lexaro2011@gmail.com>
2021-02-09 23:18:11 +0200

    ver02

--- js/script.js
+++ js/script.js
@@ -1,27 +1,36 @@
+// конструктор студента
+
 function Student(name, surName, bday) {
     this.name = name,
     this.surName = surName,
     this.bdaye = bday,
 
     this.arrayPresence = new Array(30),
-    this.arrayMark = new Array(30),
+    this.arrayMark = new Array(30)
+};
 
-    this.getAge = function() {
+// методы конструктора
+
+Student.prototype = {
+
+    getAge() {
         return (new Date).getFullYear() - this.bday;
     },
-    this.present = function() {
+    present() {
         const indexPresent = this.arrayPresence.findIndex(item => item === undefined);
         return this.arrayPresence[indexPresent] = true;
     },
-    this.absent = function() {
+    absent() {
         const indexPresent = this.arrayPresence.findIndex(item => item === undefined);
         return this.arrayPresence[indexPresent] = false;
     },
-    this.mark = function(value) {
+    mark(value) {
+        if(0 <= value || value >= 10){
         const indexMark = this.arrayMark.findIndex(item => item === undefined);
         return this.arrayMark[indexMark] = value;
+        }
     },
-    this.midleMark = function(){
+    midleMark(){
         const sumMark = this.arrayMark.reduce(function(acc, item) {
             if(item !==undefined){
                 return acc += item;
@@ -29,10 +38,33 @@ function Student(name, surName, bday) {
         },);
         
         const lengthMark = this.arrayMark.filter(item => item !== undefined)
-        return sumMark / lengthMark.length;
+        return Math.floor(sumMark / lengthMark.length)
+    },
+    midlePresent(){
+        const sumPresent = this.arrayPresence.reduce(function(acc, item) {
+            if(item !==undefined){
+                return acc += item;
+            }
+        },);
+        
+        const lengthPresent = this.arrayPresence.filter(item => item !== undefined)
+        return Math.floor(sumPresent / lengthPresent.length)
+    },
+    summary() {
+        let midleMarkValue = midleMark();
+        let midlePresentValue = midlePresent();
+        if( (midleMarkValue >= 9) && (midlePresentValue >= 0.9) ) {
+            return alert('Ути какой молодчинка!')
+        }else if( (midleMarkValue <= 9) && (midlePresentValue <= 0.9) ) {
+            return alert('Редиска!')
+        }else {
+            return alert('Норм, но можно лучше.')
+        }
     }
 }
 
+
+// вывод результатов
 const dana = new Student('dana','wait', 1985)
 console.log(dana);
 dana.present();
